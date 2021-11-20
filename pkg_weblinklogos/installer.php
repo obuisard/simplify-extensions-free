bae35bd6eb63d067eb457c8c1bdc9e767cbb6a0b
Olivier <olivier.buisard@simplifyyourweb.com>
2021-11-20 23:11:00 +0000

    v4.4.0

--- pkg_weblinklogos/installer.php
+++ pkg_weblinklogos/installer.php
@@ -14,33 +14,20 @@ use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Installer\Installer;
+use Joomla\CMS\Installer\InstallerAdapter;
 use Joomla\CMS\Installer\InstallerHelper;
+use Joomla\CMS\Installer\InstallerScript;
 use Joomla\Database\Exception\ExecutionFailureException;
 
 /**
  * Script file for the Weblink Logos Pro free module package
  */
-class Pkg_WeblinkLogosInstallerScript
+class Pkg_WeblinkLogosInstallerScript extends InstallerScript
 {
-	/**
-	 * The version number of the extension
-	 */
-	protected $release;
-
-	/**
-	 * The extension name
-	 */
-	protected $extension;
-
 	/*
 	 * Minimum extensions library version required
 	 */
-	protected $minimumLibrary = '2.0.1';
-
-	/**
-	 * Minimum Joomla! version required to install the extension
-	 */
-	protected $minimumJoomla = '4.0.0';
+	protected $minimumLibrary = '2.1.1';
 
 	/**
 	 * Available languages
@@ -60,7 +47,7 @@ class Pkg_WeblinkLogosInstallerScript
 	/**
 	 * Link to the change logs
 	 */
-	protected $changelogLink = 'https://simplifyyourweb.com/free-products/weblink-logos/file/365-weblink-logos';
+	protected $changelogLink = 'https://simplifyyourweb.com/documentation/weblink-logos/installation/updating-older-versions';
 
 	/**
 	 * Link to the translation page
@@ -73,35 +60,33 @@ class Pkg_WeblinkLogosInstallerScript
 	protected $quickstartLink = 'https://simplifyyourweb.com/documentation/weblink-logos/quickstart-guide';
 
 	/**
-	 * A list of files to be deleted
+	 * Extension script constructor
 	 */
-	protected $deleteFiles = array();
-
+	public function __construct($parent)
+	{
+	    $this->extension = 'pkg_weblinklogos';
+	    $this->minimumJoomla = '4.0.0';
+	    //$this->minimumPhp = JOOMLA_MINIMUM_PHP; // not needed
+	}
+	
 	/**
-	 * A list of folders to be deleted
-	 */
-	protected $deleteFolders = array();
-
-	/**
-	 * Called before an install/update/uninstall method
+	 * Called before any type of action
 	 *
-	 * @param string     $action     Which action is happening (install|uninstall|discover_install|update)
-	 * @param Installer  $installer  The class calling this method
+	 * @param string $action Which action is happening (install|uninstall|discover_install|update)
+	 * @param InstallerAdapter $installer The class calling this method
 	 *
 	 * @return boolean True on success
 	 */
 	public function preflight($action, $installer)
 	{
-		if ($action === 'uninstall') {
-			return true;
-		}
-
-		// make sure we are under Joomla 4.0 or over
-
-		if (version_compare(JVERSION, $this->minimumJoomla, 'lt')) {
-			Factory::getApplication()->enqueueMessage(Text::sprintf('JOOMLA_REQUIRED_VERSION', $this->minimumJoomla), 'error');
-			return false;
-		}
+	    if ($action === 'uninstall') {
+	        return true;
+	    }
+	    
+	    // checks minimum PHP and Joomla versions and that an upgrade is performed
+	    if (!parent::preflight($action, $installer)) {
+	        return false;
+	    }
 
 		// check if Weblinks component is present
 
@@ -111,35 +96,37 @@ class Pkg_WeblinkLogosInstallerScript
 			Factory::getApplication()->enqueueMessage($message, 'error');
 			//return false;
 		}
-		
-		$this->extension = $installer->getName();
-		$this->release = $installer->getManifest()->version;
 
 		// make sure the library is installed and that it is compatible with the extension
 		return $this->installOrUpdateLibrary($installer);
 	}
 
 	/**
-	 * Called on installation
+	 * method to install the component
 	 *
-	 * @return  boolean  True on success
+	 * @return boolean True on success
 	 */
-	public function install($installer) { }
+	public function install($installer) {}
 
 	/**
-	 * Called on update
+	 * method to uninstall the component
 	 *
-	 * @return  boolean  True on success
+	 * @return void
 	 */
-	public function update($installer) { }
+	public function uninstall($installer) {}
 
 	/**
-	 * Called on uninstallation
+	 * method to update the component
+	 *
+	 * @return boolean True on success
 	 */
-	public function uninstall($installer) { }
+	public function update($installer) {}
 
 	/**
-	 * method to run after an install/update/uninstall method
+	 * Called after any type of action
+	 *
+	 * @param string $action Which action is happening (install|uninstall|discover_install|update)
+	 * @param InstallerAdapter $installer The object responsible for running this script
 	 *
 	 * @return boolean True on success
 	 */
@@ -159,36 +146,31 @@ class Pkg_WeblinkLogosInstallerScript
 
    		$current_language = Factory::getLanguage()->getTag();
    		if (!in_array($current_language, $this->availableLanguages)) {
-   			//Factory::getApplication()->enqueueMessage('The ' . Factory::getLanguage()->getName() . ' language is missing for this extension.<br /><a href="' . $this->translationLink . '" target="_blank">Please consider contributing to its translation</a>', 'notice');
-   			echo '<div class="alert alert-info">The ' . Factory::getLanguage()->getName() . ' language is missing for this extension.<br /><a href="' . $this->translationLink . '" target="_blank">Please consider contributing to its translation</a>.</div>';
+   		    Factory::getApplication()->enqueueMessage('The ' . Factory::getLanguage()->getName() . ' language is missing for this component.<br /><a href="' . $this->translationLink . '" target="_blank">Please consider contributing to its translation</a> and get a license upgrade for your help!', 'info');
    		}
-
-     	// move default place-holder to /images
-
-     	$imagefiles = array();
-     	$imagefiles[] = 'placeholder_camera.png';
-
-     	$media_params = ComponentHelper::getParams('com_media');
-     	$images_path = $media_params->get('image_path', 'images');
-
-     	foreach ($imagefiles as $imagefile) {
-     	    $src = JPATH_ROOT.'/media/mod_weblinklogos/images/'.$imagefile;
-     	    $dest = JPATH_ROOT.'/'.$images_path.'/'.$imagefile;
-
-     		if (!File::copy($src, $dest)) {
-     		    Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_WARNING_COULDNOTCOPYFILE', $imagefile), 'warning');
-     		}
-     	}
 
      	if ($action === 'install') {
 
      		// link to Quickstart
 
-     		$message = Text::sprintf('PKG_WEBLINKLOGOS_INFO_LEARN', $this->quickstartLink);
-     		$message .= '<br /><br /><a href="' . $this->quickstartLink . '" target="_blank">' . HTMLHelper::image('mod_weblinklogo/quickstart.png', 'Quick Start', null, true) . '</a>';
-
-     		//Factory::getApplication()->enqueueMessage($message, 'notice');
-     		echo '<div class="alert alert-info">' . $message . '</div>';
+     	    echo '<p><a class="btn btn-primary" href="' . $this->quickstartLink . '" target="_blank"><i class="fa fa-stopwatch"></i> ' . Text::_('PKG_WEBLINKLOGOS_BUTTON_QUICKSTART') . '</a></p>';
+     		
+     		// move default place-holder to /images
+     		
+     		$imagefiles = array();
+     		$imagefiles[] = 'placeholder_camera.png';
+     		
+     		$media_params = ComponentHelper::getParams('com_media');
+     		$images_path = $media_params->get('image_path', 'images');
+     		
+     		foreach ($imagefiles as $imagefile) {
+     		    $src = JPATH_ROOT.'/media/mod_weblinklogos/images/'.$imagefile;
+     		    $dest = JPATH_ROOT.'/'.$images_path.'/'.$imagefile;
+     		    
+     		    if (!File::copy($src, $dest)) {
+     		        Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_WARNING_COULDNOTCOPYFILE', $imagefile), 'warning');
+     		    }
+     		}
      	}
 
  		// won't be an update on package first install, when moving from module to package
@@ -197,8 +179,7 @@ class Pkg_WeblinkLogosInstallerScript
 
 			// update warning
 
- 			//Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_WARNING_RELEASENOTES', $this->changelogLink), 'warning');
- 			echo '<div class="alert alert-warning">' . Text::sprintf('PKG_WEBLINKLOGOS_WARNING_RELEASENOTES', $this->changelogLink) . '</div>';
+ 		    echo '<p><a class="btn btn-primary" href="' . $this->changelogLink . '" target="_blank">' . Text::_('PKG_WEBLINKLOGOS_BUTTON_UPDATENOTES') . '</a></p>';
 
  			// overrides warning
 
@@ -253,38 +234,82 @@ class Pkg_WeblinkLogosInstallerScript
 		return true;
 	}
 
-	private function moveFile($file, $source, $destination, $minified_version = '.min')
+	private function isFolderReady($extra_path)
 	{
-		if (File::exists(JPATH_SITE . $source . '/' . $file) && !File::move(JPATH_SITE . $source . '/' . $file, JPATH_SITE . $destination . '/' . $file)) {
-			Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_ERROR_CANNOTMOVEFILE', $file), 'warning');
-		}
-
-		$file_pieces = explode('.', $file); // assumes only one . in file name
-		$file_pieces[0] .= $minified_version;
-		$file = implode('.', $file_pieces);
-
-		if (File::exists(JPATH_SITE . $source . '/' . $file) && !File::move(JPATH_SITE . $source . '/' . $file, JPATH_SITE . $destination . '/' . $file)) {
-			Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_ERROR_CANNOTMOVEFILE', $file), 'warning');
-		}
+	    $path = JPATH_SITE;
+	    $folders = explode('/', trim($extra_path, '/'));
+	    
+	    foreach ($folders as $folder) {
+	        $path .= '/' . $folder;
+	        if (!Folder::exists($path)) {
+	            if (Folder::create($path)) {
+	            } else {
+	                return false;
+	            }
+	        }
+	    }
+	    
+	    return true;
 	}
-
-	private function removeFiles()
+	
+	private function moveFile($file, $source, $destination, $minified_version = '')
 	{
-		if (!empty($this->deleteFiles)) {
-			foreach ($this->deleteFiles as $filename) {
-				if (File::exists(JPATH_SITE . $filename) && !File::delete(JPATH_SITE . $filename)) {
-					Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_ERROR_DELETINGFILEFOLDER', $filename), 'warning');
-				}
-			}
-		}
-
-		if (!empty($this->deleteFolders)) {
-			foreach ($this->deleteFolders as $folder) {
-				if (Folder::exists(JPATH_ROOT . $folder) && !Folder::delete(JPATH_ROOT . $folder)) {
-					Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_ERROR_DELETINGFILEFOLDER', $folder), 'warning');
-				}
-			}
-		}
+	    if (File::exists(JPATH_SITE . $source . '/' . $file)) {
+	        if (!$this->isFolderReady($destination) || !File::move(JPATH_SITE . $source . '/' . $file, JPATH_SITE . $destination . '/' . $file)) {
+	            Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_ERROR_CANNOTMOVEFILE', $file), 'warning');
+	        }
+	    }
+	    
+	    if ($minified_version) {
+	        $file_name = File::stripExt($file);
+	        $file_extension = File::getExt($file);
+	        $file = $file_name . $minified_version . '.' . $file_extension;
+	        
+	        if (File::exists(JPATH_SITE . $source . '/' . $file)) {
+	            if (!$this->isFolderReady($destination) || !File::move(JPATH_SITE . $source . '/' . $file, JPATH_SITE . $destination . '/' . $file)) {
+	                Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_ERROR_CANNOTMOVEFILE', $file), 'warning');
+	            }
+	        }
+	    }
+	}
+	
+	private function copyFile($file, $source, $destination)
+	{
+	    if (File::exists(JPATH_SITE . $source . '/' . $file)) {
+	        if (!$this->isFolderReady($destination) || !File::copy(JPATH_SITE . $source . '/' . $file, JPATH_SITE . $destination . '/' . $file)) {
+	            Factory::getApplication()->enqueueMessage(Text::sprintf('PKG_WEBLINKLOGOS_ERROR_CANNOTMOVEFILE', $file), 'warning');
+	        }
+	    }
+	}
+	
+	private function enableExtension($type, $element, $folder = '', $enable = true)
+	{
+	    $db = Factory::getDBO();
+	    
+	    $query = $db->getQuery(true);
+	    
+	    $query->update($db->quoteName('#__extensions'));
+	    if ($enable) {
+	        $query->set($db->quoteName('enabled').' = 1');
+	    } else {
+	        $query->set($db->quoteName('enabled').' = 0');
+	    }
+	    $query->where($db->quoteName('type').' = '.$db->quote($type));
+	    $query->where($db->quoteName('element').' = '.$db->quote($element));
+	    if ($folder) {
+	        $query->where($db->quoteName('folder').' = '.$db->quote($folder));
+	    }
+	    
+	    $db->setQuery($query);
+	    
+	    try {
+	        $db->execute();
+	    } catch (ExecutionFailureException $e) {
+	        Factory::getApplication()->enqueueMessage(Text::_('JERROR_AN_ERROR_HAS_OCCURRED'), 'error');
+	        return false;
+	    }
+	    
+	    return true;
 	}
 
 	private function getDefaultTemplate()
@@ -400,47 +425,33 @@ class Pkg_WeblinkLogosInstallerScript
 
 	private function installOrUpdatePackage($installer, $package_name, $installation_type = 'install')
 	{
-		// Get the path to the package
-
+	    // Get the path to the package
+	    
 	    $sourcePath = $installer->getParent()->getPath('source');
-		$sourcePackage = $sourcePath . '/packages/'.$package_name.'.zip';
-
-		// Extract and install the package
-
-		$package = InstallerHelper::unpack($sourcePackage);
-		$tmpInstaller = new Installer;
-
-		try {
-			if ($installation_type == 'install') {
-				$installResult = $tmpInstaller->install($package['dir']);
-			} else {
-				$installResult = $tmpInstaller->update($package['dir']);
-			}
-		} catch (\Exception $e) {
-			return false;
-		}
-
-		return true;
+	    $sourcePackage = $sourcePath . '/packages/'.$package_name.'.zip';
+	    
+	    // Extract and install the package
+	    
+	    $package = InstallerHelper::unpack($sourcePackage);
+	    if ($package === false || (is_array($package) && $package['type'] === false)) {
+	        return false;
+	    }
+	    
+	    $tmpInstaller = new Installer();
+	    
+	    if ($installation_type === 'install') {
+	        return $tmpInstaller->install($package['dir']);
+	    } else {
+	        return $tmpInstaller->update($package['dir']);
+	    }
 	}
 
+	/**
+	 * Install the library and its plugin if missing or outdated
+	 */
 	private function installOrUpdateLibrary($installer)
 	{
-		// install the library and its plugin if missing or outdated
-
 		if (!Folder::exists(JPATH_ROOT . '/libraries/syw') || !Folder::exists(JPATH_ROOT . '/plugins/system/syw')) {
-// 			if (!Folder::exists(JPATH_ROOT . '/libraries/syw')) {
-// 				if (!$this->installOrUpdatePackage($installer, 'lib_syw')) {
-// 					Factory::getApplication()->enqueueMessage(Text::_('SYWLIBRARY_INSTALLFAILED').'<br /><a href="'.$this->libraryDownloadLink.'" target="_blank">'.Text::_('SYWLIBRARY_DOWNLOAD').'</a>', 'error');
-// 					return false;
-// 				}
-// 			}
-
-// 			if (!Folder::exists(JPATH_ROOT . '/plugins/system/syw')) {
-// 				if (!$this->installOrUpdatePackage($installer, 'plg_system_syw')) {
-// 					Factory::getApplication()->enqueueMessage(Text::_('SYWLIBRARY_INSTALLFAILED').'<br /><a href="'.$this->libraryDownloadLink.'" target="_blank">'.Text::_('SYWLIBRARY_DOWNLOAD').'</a>', 'error');
-// 					return false;
-// 				}
-// 			}
 		    
 		    if (!$this->installOrUpdatePackage($installer, 'pkg_sywlibrary')) {
 		        Factory::getApplication()->enqueueMessage(Text::_('SYWLIBRARY_INSTALLFAILED').'<br /><a href="'.$this->libraryDownloadLink.'" target="_blank">'.Text::_('SYWLIBRARY_DOWNLOAD').'</a>', 'error');
@@ -452,16 +463,6 @@ class Pkg_WeblinkLogosInstallerScript
 
 			$library_version = strval(simplexml_load_file(JPATH_ADMINISTRATOR . '/manifests/libraries/syw.xml')->version);
 			if (!version_compare($library_version, $this->minimumLibrary, 'ge')) {
-
-// 				if (!$this->installOrUpdatePackage($installer, 'lib_syw', 'update')) {
-// 					Factory::getApplication()->enqueueMessage(Text::_('SYWLIBRARY_UPDATEFAILED').'<br />'.Text::_('SYWLIBRARY_UPDATE'), 'error');
-// 					return false;
-// 				}
-
-// 				if (!$this->installOrUpdatePackage($installer, 'plg_system_syw', 'update')) {
-// 					Factory::getApplication()->enqueueMessage(Text::_('SYWLIBRARY_UPDATEFAILED').'<br />'.Text::_('SYWLIBRARY_UPDATE'), 'error');
-// 					return false;
-// 				}
 			    
 			    if (!$this->installOrUpdatePackage($installer, 'pkg_sywlibrary', 'update')) {
 			        Factory::getApplication()->enqueueMessage(Text::_('SYWLIBRARY_UPDATEFAILED').'<br />'.Text::_('SYWLIBRARY_UPDATE'), 'error');
@@ -471,13 +472,6 @@ class Pkg_WeblinkLogosInstallerScript
 				Factory::getApplication()->enqueueMessage(Text::sprintf('SYWLIBRARY_UPDATED', $this->minimumLibrary), 'message');
 			}
 		}
-
-// 		if (!PluginHelper::isEnabled('system', 'syw')) {
-// 			if (!$this->enableExtension('plugin', 'syw', 'system')) {
-// 				Factory::getApplication()->enqueueMessage(Text::_('SYWLIBRARY_COULDNOTENABLEPLUGINFORLIBRARY'), 'error');
-// 				return false;
-// 			}
-// 		}
 
 		return true;
 	}
