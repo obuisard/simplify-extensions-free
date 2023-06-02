f48e5be157570e3cdd8ef35f4001b4f07d1d03ae
Olivier <olivier.buisard@simplifyyourweb.com>
2023-06-02 20:32:09 +0000

    v2.7.0

--- pkg_trulyresponsiveslides/installer.php
+++ pkg_trulyresponsiveslides/installer.php
@@ -25,7 +25,7 @@ class Pkg_TrulyResponsiveSlidesInstallerScript extends InstallerScript
 	/*
 	 * Minimum extensions library version required
 	 */
-	protected $minimumLibrary = '2.3.1';
+	protected $minimumLibrary = '2.3.3';
 
 	/**
 	 * Available languages
