ec25b99d67994b6ee0fac13de811670d047b2076
Olivier <olivier.buisard@simplifyyourweb.com>
2023-06-12 14:43:24 +0000

    v2.4.0

--- mod_library_test/mod_library_test.php
+++ mod_library_test/mod_library_test.php
@@ -19,6 +19,41 @@ if (!SYWVersion::isCompatible('2.1.0')) {
 
 // font tests
 
+$font = $params->get('font', '');
+$font2 = $params->get('font2', '');
+$weights = $params->get('fontweight', '');
+$weights2 = $params->get('fontweight2', '');
+
+$web_fonts_array = [];
+
+if (!empty($font)) {
+    $web_fonts = SYWFonts::getWebfontsFromFamily($font);
+    if (count($web_fonts) > 0) {
+        foreach ($web_fonts as $web_font) {
+            $web_fonts_array[] = ['name' => $web_font, 'weights' => (!empty($weights) ? $weights : [])];
+        }
+    }
+}
+
+if (!empty($font2)) {
+    $web_fonts = SYWFonts::getWebfontsFromFamily($font2);
+    if (count($web_fonts) > 0) {
+        foreach ($web_fonts as $web_font) {
+            $web_fonts_array[] = ['name' => $web_font, 'weights' => (!empty($weights2) ? $weights2 : [])];
+        }
+    }
+}
+
+if (count($web_fonts_array) > 0) {
+    SYWFonts::loadWebFonts($web_fonts_array);
+    if (!empty($font)) {
+        Factory::getApplication()->getDocument()->getWebAssetManager()->addInlineStyle('h2 { font-family: ' . $font . '; }');
+    }
+    if (!empty($font2)) {
+        Factory::getApplication()->getDocument()->getWebAssetManager()->addInlineStyle('h3 { font-family: ' . $font2 . '; }');
+    }
+}
+
 SYWFonts::addFontFace('Conduit ITC Light', '../../media/mod_library_test/fonts/conditcl-webfont', 'normal', 'normal', ['eot', 'svg', 'ttf', 'woff']);
 
 Factory::getApplication()->getDocument()->getWebAssetManager()->addInlineStyle('body { font-family: "Conduit ITC Light", sans-serif; }');
