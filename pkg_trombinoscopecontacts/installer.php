c86e0c39d1e27de22c36abdf4e9c2816bbf1537e
Olivier <olivier.buisard@simplifyyourweb.com>
2023-08-07 17:45:11 +0000

    v4.12.2

--- pkg_trombinoscopecontacts/installer.php
+++ pkg_trombinoscopecontacts/installer.php
@@ -170,15 +170,15 @@ class Pkg_TrombinoscopeInstallerScript extends InstallerScript
 
 			// overrides warning
 
-			$defaultemplate = $this->getDefaultTemplate();
+// 			$defaultemplate = $this->getDefaultTemplate();
 
-			if ($defaultemplate) {
-				$overrides_path = JPATH_ROOT.'/templates/'.$defaultemplate.'/html/';
+// 			if ($defaultemplate) {
+// 				$overrides_path = JPATH_ROOT.'/templates/'.$defaultemplate.'/html/';
 
-				if (Folder::exists($overrides_path.'mod_trombinoscope')) {
-					Factory::getApplication()->enqueueMessage(Text::_('PKG_TROMBINOSCOPE_WARNING_OVERRIDES'), 'warning');
-				}
-			}
+// 				if (Folder::exists($overrides_path.'mod_trombinoscope')) {
+// 					Factory::getApplication()->enqueueMessage(Text::_('PKG_TROMBINOSCOPE_WARNING_OVERRIDES'), 'warning');
+// 				}
+// 			}
 
 			// remove old cached headers which may interfere with fixes, updates or new additions
 
